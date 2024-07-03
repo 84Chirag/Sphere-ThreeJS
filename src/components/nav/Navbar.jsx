f93fdf1cc9df6829f8fbc2ac8283b6a61160af98
Chirag gour <chiragvgour@gmail.com>
2024-07-03 17:48:43 +0530

    Update Navbar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                             <a className="nav-link active" aria-current="page" href="#">Create</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Explore</a>
+                            <a className="nav-link" href="https://threejs.org/" target="_blank">Explore</a>
                         </li>
                     </ul>
                 </div>
